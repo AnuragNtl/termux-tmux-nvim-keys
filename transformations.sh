d9d653c62b806272d7265fe38520e418412ffa48
Anurag <anurag>
2025-01-11 17:08:25 +0530

    Corrections, script

--- transformations.sh
+++ transformations.sh
@@ -7,7 +7,7 @@ tr_tmux_prefix() {
 tr_tmux_command() {
     if echo "$1" | grep "TMUX_NEW_WINDOW_COMMAND" > /dev/null
     then
-        echo "${1//TMUX_NEW_WINDOW_COMMAND/$2 c . SPACE }"
+        echo "${1//TMUX_NEW_WINDOW_COMMAND/$2 c}"
     else
         echo "$1"
     fi
